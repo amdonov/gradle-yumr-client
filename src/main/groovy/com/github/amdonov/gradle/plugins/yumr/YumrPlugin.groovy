009731386c1bdc51410a39a384cf990a9fe937d8
amdonov <donovan_aaron@bah.com>
2017-02-22 12:33:45 -0500

    initial commit

---
+++ src/main/groovy/com/github/amdonov/gradle/plugins/yumr/YumrPlugin.groovy
@@ -0,0 +1,49 @@
+package com.github.amdonov.gradle.plugins.yumr
+
+import api.YumrGrpc
+import api.YumrOuterClass
+import com.google.protobuf.ByteString
+import io.grpc.ManagedChannel
+import io.grpc.netty.GrpcSslContexts
+import io.grpc.netty.NettyChannelBuilder
+import io.grpc.stub.StreamObserver
+import io.netty.handler.ssl.SslContext
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by aarondonovan on 2/21/17.
+ */
+class YumrPlugin implements Plugin<Project> {
+    void apply(Project project) {
+        project.extensions.create("yumr", YumrPluginExtension)
+        project.task('publishToYumr') {
+            doLast {
+                SslContext context = GrpcSslContexts.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build()
+                String[] conn = "${project.yumr.connection}".split(":")
+                ManagedChannel channel = NettyChannelBuilder.forAddress(conn[0], Integer.parseInt(conn[1])).
+                        sslContext(context).build()
+                api.YumrOuterClass.EmptyResponse.getDefaultInstance()
+                YumrGrpc.YumrStub stub = YumrGrpc.newStub(channel)
+                File file = new File("${project.yumr.rpm}")
+                YumrOuterClass.OpenMessage openMessage = YumrOuterClass.OpenMessage.newBuilder().setPackage(file.getName()).
+                        setRepo("${project.yumr.repo}").setSize(file.length()).build()
+                StreamObserver<YumrOuterClass.FileMessage> rpms = stub.addPackage(null)
+                // Open the package
+                rpms.onNext(YumrOuterClass.FileMessage.newBuilder().setOpenMsg(openMessage).build())
+                // Send the file in 2k chunks
+                byte[] buffer = new byte[2048]
+                file.withInputStream { is ->
+                    for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
+                        ByteString chunk = ByteString.copyFrom(buffer, 0, len)
+                        YumrOuterClass.DataMessage dataMessage = YumrOuterClass.DataMessage.newBuilder().setContent(chunk).build()
+                        rpms.onNext(YumrOuterClass.FileMessage.newBuilder().setDataMsg(dataMessage).build())
+                    }
+                }
+                // Close the package
+                rpms.onNext(YumrOuterClass.FileMessage.newBuilder().setCloseMsg(YumrOuterClass.CloseMessage.getDefaultInstance()).build())
+            }
+        }
+    }
+}
---
+++ src/main/groovy/com/github/amdonov/gradle/plugins/yumr/YumrPluginExtension.groovy
@@ -0,0 +1,10 @@
+package com.github.amdonov.gradle.plugins.yumr
+
+/**
+ * Created by aarondonovan on 2/21/17.
+ */
+class YumrPluginExtension {
+    String connection
+    String repo
+    File rpm
+}
